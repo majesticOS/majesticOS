b44c1bc197138a39d0478f08936ad32643581562
systemd <47719596+systemd1337@users.noreply.github.com>
2024-03-28 00:46:48 -0400

    Create vga.h

---
+++ vga.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "stdint.h"
+
+#define COLOR8_BLACK 0
+#define COLOR_LIGHT_GREY 7
+
+#define width 80
+#define height 25
+
+void print(const char* s);
+void scrollUp();
+void newLine();
+void Reset();
