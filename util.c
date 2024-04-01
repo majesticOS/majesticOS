d24abbd16a8da2cc9745631cacb6eacf0ffb5317
systemd <47719596+systemd1337@users.noreply.github.com>
2024-03-31 23:17:18 -0400

    Create util.c

---
+++ util.c
@@ -0,0 +1,12 @@
+#include "util.h"
+
+void memset(void *dest, char val, uint32_t count){
+
+  char *temp = (char*) dest;
+  for (; count != 0; count --){
+    *temp++ = val;
+    
+    }
+    
+   
+   }
